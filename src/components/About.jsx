c4ea79bc364bcb79bbe8967b3c2545cb2093dcd5
Bryan <bryansanudo95@gmail.com>
2023-10-10 18:13:21 -0500

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+const About = () => {
+  return <div>about</div>;
+};
+
+export default About;
